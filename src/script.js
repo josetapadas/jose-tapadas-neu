6f7b1366afbe26a5ae2781661df29f00a04d3576
José Alves <jose.tapadas@gmail.com>
2021-05-02 13:48:55 +0200

    disable debugging controls

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 import { MeshStandardMaterial } from 'three';
 
-const enable_debugging = true;
+const enable_debugging = false;
 
 // loaders
 const textureLoader = new THREE.TextureLoader()
